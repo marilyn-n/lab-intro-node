4dbcbeaa4135ea19aea4e1f8bb1b4af11e945cd8
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-02-12 13:57:04 -0600

    it's done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,47 @@
-var SortedList = function() {};
+var SortedList = function() {
+	this.items = [];
+	this.length = this.items.length
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+};
+
+SortedList.prototype.add = function(item) {
+	this.items.push(item)
+	this.length = this.items.length
+	this.items.sort(function(a, b)
+	{
+		return a - b
+	})
+  
+}
+
+SortedList.prototype.get = function(pos) {
+	this.length = this.items.length
+	return this.items[pos -1]
+}
+
+SortedList.prototype.max = function() {
+	var max = Math.max.apply(null, this.items)
+	return max
+
+}
+
+SortedList.prototype.min = function() {
+	var min = Math.min.apply(null, this.items)
+	return min
+}
+
+SortedList.prototype.average = function() {
+	var total = 0;
+	for(var i = 0; i < this.items.length; i++) {
+		total += this.items[i];
+	}
+	var avg = total / this.items.length;
+	return avg
+}
+
+SortedList.prototype.sum     = function() {
+	var sum = this.items.reduce(function(a, b) { return a + b; }, 0);
+	return sum
+}
 
 module.exports = SortedList;
